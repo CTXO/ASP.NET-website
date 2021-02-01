9e648451ead19fe8cc28b19dc03acf53b8ece308
Romero <rrcb@cin.ufpe.br>
2021-02-01 09:19:05 -0300

    Dislaying more details

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,9 +1,13 @@
 using System;
 using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace CursoMVC.Models
 {
     public class Categoria{
+
+        [Key]
         public int Id {get; set;}
 
         public string Descricao  { get; set; }
